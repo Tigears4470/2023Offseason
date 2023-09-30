d8dd3ae169089ed045d7f6a8244da77e93fc75c5
HsSteven <256787@pylusd.org>
2023-09-29 17:38:07 -0700

    Changed Turn By and

--- Mallet/src/main/java/frc/robot/commands/TurnToMatch.java
+++ Mallet/src/main/java/frc/robot/commands/TurnToMatch.java
@@ -16,7 +16,6 @@ public class TurnToMatch extends CommandBase {
         m_gyro = p_gyro;
         turnDestination = angle;
         addRequirements(m_drivetrain);
-        addRequirements(m_gyro);
     }
 
     public float getGyroZ360(){ //return the gyro position in the range 0 to 359
